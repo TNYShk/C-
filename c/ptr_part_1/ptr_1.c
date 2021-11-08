ecfc04c29aa30e284baa5995fc4639d19f4771ca
tanya.shkolnik <tatyanna.shk@gmail.com>
2021-11-08 12:45:27 +0200

    pointer part1

---
+++ c/ptr_part_1/ptr_1.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "ptr_1.h"
+
+
+
+void SwapPtrSize_t(size_t **a,size_t **b)
+{
+	SwapSizeT((size_t *)a, (size_t *)b);
+}
+
+
+void Swap2Ints(int *num1, int *num2)
+{
+	int placeholder= *num1;
+	
+	*num1= *num2;
+	*num2= placeholder;
+}
+
+
+int* CopyArray(int ar_length,int *ptr_array)
+{
+	
+	int *copyA,i;
+	copyA=(int*)malloc(ar_length*sizeof(int));
+	
+	for (i=0;i<ar_length;i++)
+	{
+		*(copyA+i)=*(ptr_array+i);
+		printf("copyA[%d] is %d\n", i,copyA[i]);
+	}
+	
+return copyA;
+}
+
+void SwapSizeT(size_t *st1,size_t *st2)
+{
+	long unsigned int holder=*st1;
+	*st1=*st2;
+	*st2=holder;
+  return;
+}
+
+
+
+
+
+
+
+	
---
+++ c/ptr_part_1/ptr_1.h
@@ -0,0 +1,17 @@
+#ifndef __PTR_1_H__
+#define __PTR_1_H__
+
+
+/****** Function Decleration ******/
+void Swap2Ints(int *,int*);			/**  gets 2 int variable adresses and swaps their value             **/
+
+int* CopyArray(int,int*);			/**  gets array size and array, returns copy of array (pointer)   **/
+
+void SwapSizeT(size_t*,size_t*);		/**  swaps 2 size_t                                         **/
+
+void SwapPtrSize_t(size_t **,size_t **);	/** swaps 2 pointer to size_t                    **/
+
+
+
+#endif /* end of ptr_1.h */
+
