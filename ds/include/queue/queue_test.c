defcd17757c41c5795e70dc39630b82ceced9462
tanya.shkolnik <tatyanna.shk@gmail.com>
2021-12-15 11:59:25 +0200

    no time to test

--- ds/include/queue/queue_test.c
+++ ds/include/queue/queue_test.c
@@ -6,11 +6,15 @@
 #include "sll.h"
 #include "queue.h"
 
+/*testing is a mass not complete! */
+
+
 
 int main(void)
 {
 	
 	queue_t *qt = NULL;
+	queue_t *qt2 = NULL;
 
 	void *ptr = NULL; 
 	void *peek = NULL;
@@ -20,9 +24,11 @@ int main(void)
 	float f = 3.14;
 
 	qt = QueueCreate();
+	qt2 = QueueCreate();
 	(QueueIsEmpty(qt) == 1) ? printf("Q is Empty\n") : printf("Q has been stuffed\n");
 	ptr = &f;
 	printf("enqueing %d\n", QueueEnqueue(qt,ptr));
+	printf("\nenqueing to qt2 (%d)\n\n", QueueEnqueue(qt2,ptr));
 	(QueueIsEmpty(qt) == 1) ? printf("Q is Empty\n") : printf("Q has been stuffed\n");
 	
 	peek = QueuePeek(qt);
@@ -30,22 +36,37 @@ int main(void)
 
 	ptr = &t;
 	printf("enqueing %d\n", QueueEnqueue(qt,ptr));
-	printf("size? %ld\n",QueueSize(qt));
+	printf("\nenqueing to qt2 (%d)\n\n", QueueEnqueue(qt2,ptr));
+	
+
+	printf("qt2 size? %ld\n",QueueSize(qt2));
+	printf("qt size? %ld\n",QueueSize(qt));
+
+	QueueAppend(qt,qt2);
+
+	printf("Appended qt2 into qt1\n");
 	peek = QueuePeek(qt);
-	printf("peekaboo! top %ld\n", *(size_t*)peek);
+	printf("\npeekaboo!  %f\n", *(float *)peek);
 	t = 666;
-	printf("enqueing %d\n", QueueEnqueue(qt,ptr));
-	printf("size? %ld\n",QueueSize(qt));
+	printf("enqueing qt1 %d\n", QueueEnqueue(qt,ptr));
 
 	QueueDequeue(qt);
+	printf("post dequeue qt1 size? %ld\n",QueueSize(qt));
+	printf("enqueing qt1 %d\n", QueueEnqueue(qt,ptr));
+	printf("size? %ld\n",QueueSize(qt));
+	QueueDequeue(qt);
+	printf("dequeue size? %ld\n",QueueSize(qt));
+
+	
 	peek = QueuePeek(qt);
-	printf("peekaboo! top %ld\n", *(size_t*)peek);
+	printf("peekaboo!  %ld\n", *(size_t*)peek);
 
 
 	
 
 
 	QueueDestroy(qt);
+	QueueDestroy(qt2);
 
 	return 0;
 }
