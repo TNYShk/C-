5f08e30c6bf499621cf43b60cf5200a973009154
tanya.shkolnik <tatyanna.shk@gmail.com>
2021-12-16 14:03:41 +0200

    updated header sll

--- ds/sll/sll.h
+++ ds/sll/sll.h
@@ -4,7 +4,7 @@
 #include <stddef.h>  /* size_t */
 
 typedef struct slist slist_t;
-typedef struct slist_node* slist_iter_t;    /* current pointer*/
+typedef struct slist_node* slist_iter_t;    
 
 typedef int (*match_func_t)(void *data, void *param);
 typedef int (*action_func_t)(void *data, void *param);
@@ -14,44 +14,34 @@ typedef int (*action_func_t)(void *data, void *param);
 
 /* time complexity: O(1), space complexity O(1) 
 
-initializes sll
+initializes the list
 returns pointer to the created list or NULL if memory allocation failed.
-ahm ahm free memory after use
+! free memory after use !
 */
-
 slist_t *SListCreate(void);
+
 
 /* time complexity: O(n), space complexity O(1) 
 frees all allocated nodes
 no return value
-	
 */
 void SListDestroy(slist_t *slist);
 
 
-
-
 /* time complexity: O(1), space complexity O(1) 
+amortized complexity! **if malloc fails*** do need to run on the sll to find and return End which is O(n) 
 inserts new node right befor the requsted node (where)
-
-returns the node that was added
-
+returns the node that was added, or last node of the list if failed
 */
 slist_iter_t SListInsertBefore(const slist_iter_t where, void *data); 
 
 
-
-
-
 /* time complexity: O(1), space complexity O(1) 
 amortized complexity! **if malloc fails*** do need to run on the sll to find and return End which is O(n) 
-returns node that was inserted
+inserts new node right after the where node
+returns node that was inserted, or last node of the list if failed
 */
 slist_iter_t SListInsertAfter(slist_iter_t where,void *data);
-
-
-
-
 
 
 /* time complexity: O(1), space complexity O(1) 
@@ -61,17 +51,11 @@ returns the node at the same location (not the same node)
 slist_iter_t SListRemove(slist_iter_t iterator);
 
 
-
-
-
 /* time complexity: O(n), space complexity O(1) 
 counts amount of current nodes in the sll
 returns amount of current nodes
 */
 size_t SListCount(const slist_t *slist);
-
-
-
 
 
 /* time complexity: O(1), space complexity O(1) 
@@ -82,35 +66,30 @@ size_t SListCount(const slist_t *slist);
 int SListIsEmpty(const slist_t *slist);
 
 
-
-
-/* time complexity: O(n), space complexity O(1) */
+/* time complexity: O(n), space complexity O(1) 
+	find the node based on given parameter
+	if not found, returns End node
+*/
 slist_iter_t SListFind(const slist_iter_t from, const slist_iter_t to, match_func_t is_match, void *param); 
 
 
-
-
-
-/* time complexity: O(n), space complexity O(1) */
+/* time complexity: O(n), space complexity O(1) 
+	do certein function using a given param on a set of nodes (from-to)
+	return SUCCESS of FAIL
+*/
 int SListForEach( slist_iter_t from, slist_iter_t to, action_func_t action_func, void *param);
 
 
-
-
 /* time complexity: O(1), space complexity O(1) 
-	
-	returns Begin Iterator, points to the first node in sll
+	returns Begin iterator, points to the first node in sll
 */
 slist_iter_t SListBegin(const slist_t *slist);
 
 
-
-/*point to dummy */
 /* time complexity: O(1), space complexity O(1) 
- return the dummy. which always the last element in the sll
+ return the last element in the sll
 */
 slist_iter_t SListEnd(const slist_t *slist); 
-
 
 
 /* time complexity: O(1), space complexity O(1) 
@@ -119,12 +98,10 @@ return the next node in the list, unless its the end, then returns iterator
 slist_iter_t SListNext(const slist_iter_t iterator);
 
 
-
 /* time complexity: O(1), space complexity O(1) 
 returns the saved data in the requested node
 */
 void *SListGetData(const slist_iter_t iterator);
-
 
 
 /* time complexity: O(1), space complexity O(1) 
@@ -133,8 +110,8 @@ no return value
 */
 void SListSetData(slist_iter_t iterator, void *data);
 
-/* time complexity: O(1), space complexity O(1) 
 
+/* time complexity: O(1), space complexity O(1) 
 checks wether 2 nodes point to the same location
 1 if true
 */
@@ -142,8 +119,8 @@ int SListIsEqual(const slist_iter_t iterator1, const slist_iter_t iterator2);
 
 
 /* time complexity: O(1), space complexity O(1)
-** appends src to the end of dest, while maintating src viability (stand alone)
-
+** appends src to the end of dest, while maintating src viability (stand alone),
+post append, the src size is zero
 */
 void SlistAppend(slist_t *dest, slist_t *src);
 
