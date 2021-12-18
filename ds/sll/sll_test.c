29f812ae1520ee2cd5124408dd933048b0610d9f
tanya.shkolnik <tatyanna.shk@gmail.com>
2021-12-18 18:04:21 +0200

    list not q

--- ds/sll/sll_test.c
+++ ds/sll/sll_test.c
@@ -63,7 +63,7 @@ void TestOne()
 	third = SListInsertAfter(second, &ff);
 	assert (2 == SListCount(twinky));
 	float_ptr = SListGetData(third);
-	printf("inserted After first, 2rd value is %f\n",*(float *)float_ptr);
+	printf("inserted After first, 2nd value is %f\n",*(float *)float_ptr);
 	assert(*(float*)float_ptr == ff);
 	
 	assert(SListIsEqual(start, second) == 1);
@@ -129,61 +129,66 @@ void TestTwo()
 
 	second = SListInsertBefore(start, &t);
 	int_ptr = SListGetData(second);
-	printf("inserted 1st value to twinky, value is %d\n",*(int *)int_ptr);
+	printf("inserted 1st value to List1, value is %d\n",*(int *)int_ptr);
 	
 	k_second = SListInsertBefore(k_start, &t);
 	int_kptr = SListGetData(k_second);
-	printf("inserted 1st value to kika, = value is %d\n",*(int *)int_kptr);
-	printf("\n\t********************Asserting Inserted******************\n");
+	printf("inserted 1st value to List2, = value is %d\n",*(int *)int_kptr);
+	printf("\n\t******************** Asserting Inserted ******************\n");
 	assert(second != k_second);
-	printf("\n\t********************adding nodes******************\n");
+	printf("\n\t******************** Adding nodes ******************\n");
 	third = SListInsertBefore(second, &ttt);
 	int_ptr = SListGetData(third);
-	printf("inserted 2st value to Q1, Before first = value is %d\n",*(int *)int_ptr);
+	printf("inserted 2st value to List1, Before first = value is %d\n",*(int *)int_ptr);
 
 	k_end = SListInsertAfter(k_second, &tt);
 	int_kptr = SListGetData(k_end);
-	printf("inserted 2st value to Q2, After first = value is %d\n",*(int *)int_kptr);
+	printf("inserted 2st value to List2, After first = value is %d\n",*(int *)int_kptr);
 
-	printf("num of elements in Q1 is %ld\n",SListCount(twinky));
-	printf("num of elements in Q2 is %ld\n",SListCount(kika));
+	printf("num of elements in List1 is %ld\n",SListCount(twinky));
+	printf("num of elements in List2 is %ld\n",SListCount(kika));
 	
 
-	printf("\n\t********************Testing Add Func******************\n");
+	printf("\n\t******************** Testing Add Func ******************\n");
 	int_kptr = SListGetData(k_start);
-	printf("Pre add Q2 1st node value is %d\n",*(int *)int_kptr);
+	printf("Pre add List2 1st node value is %d\n",*(int *)int_kptr);
 
 	int_kptr = SListGetData(k_end);
-	printf("Pre add Q2 2st node value is %d\n",*(int *)int_kptr);
+	printf("Pre add List2 2st node value is %d\n",*(int *)int_kptr);
 	SListForEach(SListBegin(kika), SListEnd(kika),AddSome, &t);
 
 	int_kptr = SListGetData(k_start);
-	printf("POST Add Func to Q2, 1st node value is %d\n",*(int *)int_kptr);
+	printf("POST Add Func to List2, 1st node value is %d\n",*(int *)int_kptr);
 
 	int_kptr = SListGetData(k_end);
-	printf("POST Add Func to Q2, 2st node value is %d\n",*(int *)int_kptr);
+	printf("POST Add Func to List2, 2st node value is %d\n",*(int *)int_kptr);
 
-	printf("\n\t********************Append Q1 into Q2******************\n");
+	printf("\n\t******************** Append List1 into List2 ******************\n");
 
 	SlistAppend(kika, twinky);
-	printf("\npost append number of nodes Q2: %lu\n",SListCount(kika));
-	printf("\npost append number of nodes Q1: %lu\n",SListCount(twinky));
+	printf("\npost append number of nodes List2: %lu\n",SListCount(kika));
+	printf("\npost append number of nodes List1: %lu\n",SListCount(twinky));
 
-	printf("\n\t*************************Adding nodes to Q1***********************\n");
+	printf("\n\t************************* Adding nodes to List1 ***********************\n");
 	append = SListBegin(twinky);
 	t = 26;
 	append = SListInsertBefore(append,&t);
 	int_ptr = SListGetData(append);
-	printf("inserted node to Q1, its value  %d\n",*(int *)int_ptr);
-	printf("\nPost Append and insertion into Q1 count is: %lu\n",SListCount(twinky));
+	printf("inserted node to List1, its value  %d\n",*(int *)int_ptr);
+	printf("\nPost Append and insertion into List1 count is: %lu\n",SListCount(twinky));
 	assert(SListCount(kika) != SListCount(twinky));
-	printf("\nPost Append number of nodes Q2: %lu\n",SListCount(kika));
-	printf("\n\t*************************Adding nodes to Q2***********************\n");
+	printf("\nPost Append number of nodes List2: %lu\n",SListCount(kika));
+	printf("\n\t************************* Adding nodes to List2 ***********************\n");
 	k_append = SListInsertBefore(k_end, &t);
 	int_kptr = SListGetData(k_append);
-	printf("POST Append, adding node to Q2, its value is %d\n",*(int *)int_kptr);
-	printf("\nnumber of nodes Q2, post add: %lu\n",SListCount(kika));
-	printf("\nnumber of nodes in Q1:  %lu\n",SListCount(twinky));
+	printf("POST Append, adding node to List2, its value is %d\n",*(int *)int_kptr);
+	printf("\nnumber of nodes List2, post add: %lu\n",SListCount(kika));
+	printf("\nnumber of nodes in List1:  %lu\n",SListCount(twinky));
+
+	printf("\n\t********************** Remove node from List2 ***********************\n");
+	k_append = SListRemove(k_end);
+	printf("\nnumber of nodes List2, post remove: %lu\n",SListCount(kika));
+
 	SListDestroy(twinky);
 	SListDestroy(kika);
 
