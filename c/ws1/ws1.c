36630383aaa324dd90d50d49e4e7a830839d19f9
tanya.shkolnik <tatyanna.shk@gmail.com>
2021-12-26 09:10:10 +0200

    beasutify

--- c/ws1/ws1.c
+++ c/ws1/ws1.c
@@ -7,15 +7,15 @@
 /* **Exercise** 4 */
 void HelloHexa()
 {
-	char phrase[]={"\"Hello World!\""};
+	char phrase[] = {"\"Hello World!\""};
 
 	char *ptr = phrase;
-	printf("%s\n",phrase);
+	printf("%s\n", phrase);
 	
 	while (*ptr)
 	{
 		printf("0x%x ",*ptr);
-		ptr++;
+		++ptr;
 	}
 	printf("\n");
 }
@@ -23,19 +23,19 @@ void HelloHexa()
 /* **Exercise** 5 */
 double PowerofTen(int exponent)
 {
-	double base=10.0;
-	double ans=1.0;
+	double base = 10.0;
+	double ans = 1.0;
 	
-	if (0>exponent)
+	if (0 > exponent)
 	{
-	   exponent=-exponent;
-	   base=1.0/base;
+	   exponent = -exponent;
+	   base = 1.0 / base;
 	}
 	
 	while(exponent)
 	{
-	   ans*=base;
-	   exponent--;
+	   ans *= base;
+	   --exponent;
 	}
 	return ans;
 }
@@ -45,10 +45,10 @@ void PrintReverseNum(int number)
 {
    printf("%d shall revese! to: ",number);
    
-   while(number!=0)
+   while(number != 0)
    {
-	printf("%d",number%10);
-	number/=10;
+	printf("%d",number % 10);
+	number /= 10;
    }
    
    printf("\n");
@@ -57,29 +57,29 @@ void PrintReverseNum(int number)
 /* **Ex 6-Vers 2 */
 int ReverseInt(int num)
 {
-   int sign=1;
-   int ans=0;
-   if ( 0> num)
+   int sign = 1;
+   int ans = 0;
+   if (0 > num)
    {
-     num*=-1;
-     sign= -1;
+     num *= -1;
+     sign = -1;
    }
    
    while(num)
    {
 	
-	ans=ans * 10 + (num%10);
-	num/=10;
+	ans = ans * 10 + (num%10);
+	num /= 10;
    }
   return ans*sign;	
 }
 
 /* **Exercise** 7 */
-void Swap(int*a, int*b)
+void Swap(int *a, int *b)
 {
-   int holder=*a;
-   *a=*b;
-   *b=holder;
+   int holder = *a;
+   *a = *b;
+   *b = holder;
 }
 
 
