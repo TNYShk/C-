471998037a3d65cb15f52027cb89215d4cecc7ef
tanya.shkolnik <tatyanna.shk@gmail.com>
2021-12-28 16:20:22 +0200

    testing changes

--- projects/scheduler/schedule_test.c
+++ projects/scheduler/schedule_test.c
@@ -27,21 +27,23 @@ void TestSix();
 int Action(void *task_args);
 int FAction(void *task_args);
 int RAction(void *task_args);
+
 int RemoveTaskAction(void *task_args);
 
-/*static int StopAction(void *task_args);*/
+/* StopAction funcs for testing;*/
 static int StopAction4(void *task_args);
 static int StopAction3(void *task_args);
 static int StopAction6(void *task_args);
 
 static int CreateAction(void *task_args);
-
+/* StopVoid funcs for testing;*/
 static void StopVoid(void *cleanup_args);
 static void StopVoid4(void *cleanup_args);
 static void StopVoid3(void *cleanup_args);
-
-
 void CleanT(void *cleanup_args);
+
+
+
 
 
 int Action(void *task_args)
@@ -118,7 +120,7 @@ static int StopAction6(void *task_args)
 int RemoveTaskAction(void *task_args)
 {
 	SchedRemoveTask( ((remove_t *)task_args)->sched, ((remove_t *)task_args)->uid );
-	printf("got here\n");
+	printf("Remove task action!\n");
 
 	return 0;
 }
@@ -149,7 +151,7 @@ static void StopVoid3(void *cleanup_args)
 	if (NULL != stop)
 	{
 		SchedStop((scheduler_t *)cleanup_args);
-		printf("stop void\n");
+		printf("stop void3\n");
 	}
 	fclose(stop);	
 }
@@ -159,7 +161,7 @@ static void StopVoid4(void *cleanup_args)
 	if (NULL != stop)
 	{
 		SchedStop((scheduler_t *)cleanup_args);
-		printf("stop void\n");
+		printf("stop void4\n");
 	}
 	fclose(stop);	
 }
@@ -170,21 +172,15 @@ int main(void)
 {
 	TestOne();
 	TestTwo();
-	TestThree();
 	TestFour();
+	TestThree();
+	
 	
 	
 	TestFive();
 	TestSix();
 	
 	
-	/*
-	TestFour();
-	TestFive();
-	TestThree();
-	TestOne();
-	TestTwo();
-	*/
 	
 	
 
@@ -205,7 +201,7 @@ void TestOne()
 	printf("\n\t------------------------------Test1---------------------------\n");
 	printf("created sched, size is %ld\n", SchedSize(new_sched));
 
-	test_uid = SchedAddTask(new_sched, &Action, &x, &CleanT, &x, time(NULL));
+	test_uid = SchedAddTask(new_sched, &Action, &x, NULL, NULL, time(NULL));
 	printf("added task to sched, size is %ld\n", SchedSize(new_sched));
 	
 	assert(0 == UIDIsSame(test_uid, UIDBadUID));
@@ -213,7 +209,7 @@ void TestOne()
 	assert(0 == SchedIsEmpty(new_sched));
 
 
-	test2_uid = SchedAddTask(new_sched, &Action, &y, &CleanT, &y, time(NULL) - 601);
+	test2_uid = SchedAddTask(new_sched, &Action, &y, NULL, NULL, time(NULL) - 601);
 	
 	printf("added another task to sched, size is %ld\n", SchedSize(new_sched));
 	assert(0 == UIDIsSame(test2_uid, UIDBadUID));
@@ -242,7 +238,7 @@ void TestTwo()
 	printf("\tcheck: Creates Scheduler, Add & Remove task, size, clear \n");
 	printf("created Scheduler, size is %ld\n", SchedSize(new_sched));
 
-	test_uid = SchedAddTask(new_sched, &Action, &x, &CleanT, &x, time(NULL));
+	test_uid = SchedAddTask(new_sched, &Action, &x, NULL, NULL, time(NULL));
 	printf("added task to sched, size is %ld\n", SchedSize(new_sched));
 	
 	assert(0 == UIDIsSame(test_uid, UIDBadUID));
@@ -314,6 +310,7 @@ void TestFour()
 
 	printf("\n\t------------------------------Test4---------------------------\n");
 	printf("\tcheck: Creates Scheduler, Actions: Stop Action, Void Stop \n");
+	
 	SchedAddTask(new_sched, &Action, &x, &CleanT, &y, time(NULL) + 1);
 	SchedAddTask(new_sched, &Action, &y,&StopVoid4, (void *)new_sched, time(NULL) + 2);
 	SchedAddTask(new_sched, &RAction, &y, NULL,NULL, time(NULL) );
