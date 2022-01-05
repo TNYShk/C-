00fc932d13241091426642460eee2d727ab12d53
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-01-05 12:25:44 +0200

    rewrite init, alloc, chunk

--- system_programming/vsa/vsa.c
+++ system_programming/vsa/vsa.c
@@ -8,6 +8,7 @@
 #include <stdlib.h> /* size_t, labs  */
 #include <assert.h> /* asserts */
 #include <stdio.h>
+#include <limits.h> /* for LONG_MAX, LONG_MIN */
 
 #include "vsa.h" /* header file */
 
@@ -19,6 +20,7 @@
 #define ALIGNUP(a) ((a + WORD_SIZE - 1) & -(WORD_SIZE))
 #define ZERO (0l)
 #define MAGIC (0x666AAA666AAA6666l)
+#define STOP (3)
 
 enum status
 {
@@ -33,14 +35,15 @@ struct vsa
 
 #ifdef DEBUG
 	long magic;
+	
 #endif
 
 };
 
 
 
-
-
+static int DefragPool(vsa_t *pool);
+static vsa_t *GetNext(vsa_t *pool);
 
 /*pseudo
 	using a runner, create a block guard at the end of the space 
@@ -51,20 +54,20 @@ struct vsa
 */
 vsa_t *VSAInit(void *pool, size_t mem_size)
 {
-	char *runner = NULL;
 	
+	char *runner = NULL;
+
 	assert (NULL != pool);
 	assert (mem_size >= WORD_SIZE * WORD_SIZE);
-
-	mem_size -=sizeof(vsa_t);
 	
+	mem_size -= sizeof(vsa_t);
+	runner = (char *)pool + mem_size;
+	
+	((vsa_t*)runner)->start = STOP;
 	((vsa_t*)pool)->start = mem_size - sizeof(vsa_t);
-
-	runner = ((vsa_t*)pool)->start + (char *)pool;
 	
-	*(long *)runner = END;
+	return (vsa_t*)pool;
 
-	return (vsa_t *)pool;
 }
 
 
@@ -79,147 +82,71 @@ void VSAFree(void *block)
 	}
 }
 
-/*PSEUDO
-	
-	
-	align alloc_size
-	run biggest chunk func (has defrag in it)
-	verify that alloc_size < biggest chunk
-	run from pool start until *BH = 0 (end) or BH > alloc_size
-	perform allocation- save current BH value in temp var,
-	update found BH to -alloc_size
-	move +alloc_size and create a new BH,
-	 
-	 return temp var adrs
- */
+
 void *VSAAlloc(vsa_t *pool, size_t alloc_size)
 {
-	vsa_t *allocated = ((vsa_t *)(char *)pool);
-	
+	vsa_t *allocated = pool;
+	long temp = allocated->start;
+	printf("temp is %ld\n",temp );
 	assert(NULL != pool);
-	assert(ZERO < alloc_size);
-
-	alloc_size = ALIGNUP(alloc_size);
+	assert(WORD_SIZE < alloc_size);
 	
+	alloc_size = ALIGNUP(alloc_size) + sizeof(vsa_t);
+/*
 	if (alloc_size < VSALargestFreeChunck(pool))
 	{
-		while( (END != ((vsa_t *)allocated)->start ) && ( ((vsa_t *)allocated)->start < (long)alloc_size) )
+		*/
+		while( allocated->start < ( (long)(alloc_size )) )
 		{
-			*((char **)&allocated) += labs(((vsa_t *)allocated)->start);
+			allocated = GetNext(allocated);
+			printf("Alloc_size is %ld\n",alloc_size );
 		}
-		
+
+		allocated->start = -1 * ((long)alloc_size - sizeof(vsa_t));
+
+		*(long *)((char *)allocated + alloc_size ) = temp - alloc_size ;
+
 		
 
-	*(long *)(((char **)&allocated) + sizeof(vsa_t)) -=  alloc_size;
-
-		*(long *)allocated = -1 * (long)alloc_size;
-					printf("VSAAlloc: allocated %ld \n", *(long*)allocated);						
-		
-						printf("VSAAlloc: allocated->start is %ld \n", allocated->start);	
-	}
-	
 	return allocated;	
+	
+	
 }
 
 
 
-	/*
-	if(VSALargestFreeChunck(pool) > alloc_size)
-	{
-		printf("VSAAlloc: before slow runner is %ld \n", *(long *)slow_runner);
-			printf("VSAAlloc: before, pool->start is %ld \n", pool->start);
-		while ( (ZERO != pool->start) && ( (*(long *)slow_runner) < (long)alloc_size ) )
-		{
-			long temp = labs(*(long *)slow_runner);
-			slow_runner += temp;
-			pool = (vsa_t *)((char *)pool + temp);
-			printf("temp is %ld\n", temp);
-		}
-		pool->start -= alloc_size; 
-		printf("VSAAlloc: pool->start is %ld \n", pool->start);
-		*(long *)slow_runner = -alloc_size;
-		printf("VSAAlloc:  slow runner is %ld \n", *(long *)slow_runner);
-	}
-		printf("VSAAlloc: post pool->start is %ld \n", pool->start);
-		printf("VSAAlloc: post slow runner is %ld \n", *(long *)slow_runner);
-	return slow_runner;
-	*/
 
 	
-
-
-
-
-/*
-PSEUDO
- using slow & fast runners check for positive value in each BH
-		perform merge- update first BH to be old value + next BH
- */
-
-
 
 
 
 static int DefragPool(vsa_t *pool)
 {
-	vsa_t *fast_runner = (vsa_t *)((char *)pool + pool->start);
-	vsa_t *slow_runner = ((vsa_t *)(char *)pool);
-	
-	
-	while (*(long *)fast_runner != END)
+	vsa_t *slow = pool;
+	vsa_t *fast = GetNext(slow);
+	if ( (slow->start > ZERO) && (fast->start > ZERO))
 	{
-		printf("defrag:fast runner is %ld \n", *(long *)fast_runner);
-		printf("defrag:slow runner is %ld \n", *(long *)slow_runner);
-		if ((*(long *)slow_runner > ZERO) && (*(long *)fast_runner > ZERO))
-		{
-			*((char **)&slow_runner) += fast_runner->start ;
-			return SUCCESS;
-		}
-		
-		*((char **)&fast_runner) += labs(fast_runner->start);
-		*((char **)&slow_runner) += labs(slow_runner->start);
-	
+		pool = GetNext(pool);
+		return SUCCESS;
 	}
+					
 	return END;
-
-
-	/*vsa_t *fast_runner = (vsa_t *)((char *)pool) ;
-
-	printf("defrag before while: fast runner is %ld \n", *(long *)fast_runner);
-
-		if (ZERO < fast_runner->start)
-		{
-			printf("defrag:fast runner->start is %ld \n", fast_runner->start);
-			printf("defrag before fast runner is %ld \n", *(long *)fast_runner);
-			fast_runner += labs(fast_runner->start);
-			printf("defrag:fast runner->start is %ld \n", fast_runner->start);
-			printf("defrag: fast runner is %ld \n", *(long *)fast_runner);
-			return SUCCESS;
-		}
-
-	return END;*/
 }
 
 
 size_t VSALargestFreeChunck(vsa_t *pool)
 {
-	size_t chunk = ZERO;
-	vsa_t *chunk_runner = ((vsa_t *)(char *)pool);
-	
-	printf("CHUNK: chunk->start is %ld \n", chunk_runner->start);
-	while (END != chunk_runner->start)
-	{
-		if(ZERO < chunk_runner->start)
-		{
-			DefragPool(chunk_runner);	
+	long chunk = ZERO;
 
-			if(chunk_runner->start >= (long)chunk )
-			{
-				chunk = chunk_runner->start;
-			}	
-		}
-		printf("CHUNK:  is %ld \n", chunk);
-	*((char **)&chunk_runner) += labs(chunk_runner->start);
+	while (pool->start != STOP)
+	{
+		DefragPool(pool);	
+		if(pool->start >= chunk )
+		{
+			chunk = pool->start;
+		}	
+
+	pool = GetNext(pool);
 			
 	}
 
@@ -227,6 +154,13 @@ size_t VSALargestFreeChunck(vsa_t *pool)
 }
 	  
 	
+
+static vsa_t *GetNext(vsa_t *pool)
+{
+	*(char **)&pool += labs(pool->start) + sizeof(vsa_t);
+	
+	return pool;
+}
 
 
 
--- system_programming/vsa/vsa_test.c
+++ system_programming/vsa/vsa_test.c
@@ -8,37 +8,50 @@
 #include <stdlib.h> /* size_t  */
 #include <assert.h> /* asserts */
 #include <stdio.h> /* printf */
-
+#include <limits.h> /* for LONG_MAX, LONG_MIN */
 #include "vsa.h" /* header file */
 
 
 #define WORD_SIZE (sizeof(size_t))
 #define ALIGNUP(a) ((a + WORD_SIZE - 1) & -(WORD_SIZE))
+#define MAGIC (0x666AAA666AAA6666l)
+#define STOP (3)
 
 void VSAPrint(vsa_t *vsa);
 
 int main(void)
 {
-    size_t ss = 124;
-    void *aloc_ptr = malloc(124);
+   size_t ss = 0;
+    void *aloc_ptr = malloc(128);
     vsa_t *test = NULL;
 
     void *alloc_test = NULL;
    /* printf("adrs of aloc_ptr id %p \n", aloc_ptr);*/
-    ss = ALIGNUP(124);
-    printf("aligned up is %ld\n", ss);
-    test = VSAInit(aloc_ptr, ss); 
-
+   
+    
+    test = VSAInit(aloc_ptr, 128); 
     VSAPrint(test);
+     alloc_test = VSAAlloc(test, 32);
+     ss = VSALargestFreeChunck(test);
+      printf("largest chunk is %ld\n", ss);
+       VSAPrint(test);
+    /*
+     alloc_test = VSAAlloc(test, 33);
+    printf("adrs %ld\n",*((long*)test) );
+
+     printf("adrs %ld\n",*((long*)((char*)test + 120)) );
+    VSAPrint(test);
+   
     ss = VSALargestFreeChunck(test);
     printf("largest chunk is %ld\n", ss);
     
     alloc_test = VSAAlloc(test, 33);
    
+   /*
     ss = VSALargestFreeChunck(test);
     printf("largest chunk is %ld\n", ss);
-    
-    VSAPrint(test);
+    */
+  
  
     free(aloc_ptr);
     return 0;
@@ -51,11 +64,16 @@ void VSAPrint(vsa_t *vsa)
     runner = vsa;
 
     printf("------VSA is:-------\n");
-    while (0 != *(long *)runner)
+    while (STOP != *(long *)runner)
     {
         ++counter;
-        printf("%lu. Size is %ld\n", counter, *(long *)runner);
-        if (*(long *)runner < 0)
+        printf("%lu. Address of header is %p, Size of block is %ld\n", counter, (void *)runner, *(long *)runner);
+
+        if (*(long *)runner == 0)
+        {
+            runner = (void *)((char *)runner + WORD_SIZE);
+        }
+        else if (*(long *)runner < 0)
         {
             runner = (void *)((char *)runner + *(long *)runner * (-1) + sizeof(size_t));
         }
@@ -64,4 +82,5 @@ void VSAPrint(vsa_t *vsa)
             runner = (void *)((char *)runner + *(long *)runner + sizeof(size_t));
         }
     }
+    printf("\n");
 }
