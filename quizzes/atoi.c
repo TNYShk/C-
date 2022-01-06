5bf2aeffbf364c849225cea8ffc09f58ac271f5f
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-01-06 09:16:22 +0200

    fixed atoi added another ver

--- quizzes/atoi.c
+++ quizzes/atoi.c
@@ -9,23 +9,24 @@
 #define DECIMAL (10)
 #define CONVERT_NEG (-1)
 
-int AtoI(const char *string);
-
+int AtoI(const char *string, int base);
+int AToI(const char *string);
 
 
 int main(void)
 {
 	char *test = "6f0";
-
-	printf("%s converted to int is %d\n", test,AtoI(test));
+	char *test1 = "666";
+	printf("%s converted to int is %d\n", test,AtoI(test,16));
 	printf("%c is %c\n", 'c', toupper('c'));
-	
+
+	printf("%s converted to int base 10 is %d\n", test1,AToI(test1));
 	return 0;
 }
 
-int AtoI(const char *string)
+int AtoI(const char *string, int base)
 {
-	int answer = 1;
+	int answer = 0;
 	int sign = 1;
 
 	assert(NULL != string);
@@ -45,14 +46,41 @@ int AtoI(const char *string)
 		if(isdigit(*string))
 		{
 			
-			answer = (answer * DECIMAL) + *string - ZERO;
+			answer = (answer * base) + *string - ZERO;
 		}
 		else
 		{
-			answer = (answer * DECIMAL) + toupper(*string) - CAP_LETTER + DECIMAL;
+			answer = (answer * base) + toupper(*string) - CAP_LETTER + DECIMAL;
 		}
 		++string;
 	}
 	
+	return sign * answer;
+}
+
+int AToI(const char *string)
+{
+	int answer = 0;
+	int sign = 1;
+
+	assert(NULL != string);
+
+	while(isspace(*string))
+	{
+		++string;
+	}
+	
+	if (*string == '-')
+	{
+		sign = CONVERT_NEG;
+		++string;
+	}
+	
+	while(isdigit(*string))
+	{
+		answer = (answer * DECIMAL) + *string - ZERO;
+		++string;
+	}
+		
 	return sign * answer;
 }
