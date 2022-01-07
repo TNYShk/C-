7ef891f5d4f97fb6e360e40802bfa8687fc74fbf
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-01-07 14:37:03 +0200

    updates. ds exam

--- c/wboai/wboai.c
+++ c/wboai/wboai.c
@@ -9,6 +9,9 @@
 
 #define DECIM (10)
 #define ASCII (256)
+#define ZERO ('0')
+#define CAP_LETTER ('A')
+
 #define MIN2(a,b) ((a)<(b)?(a):(b))
 
 #define IS_LITTLE_ENDIAN (*(int *)&("=1") != 0 ) 
@@ -19,30 +22,154 @@
 void PrintFromArrays(char *a, char *b, char *c, size_t a_arr, size_t b_arr, size_t c_arr)
 {
 	
-	size_t i=0;
-	int ascii[ASCII] ={0};
+	size_t i = 0;
+	int ascii[ASCII] = {0};
 
 	assert (NULL != a);
 	assert (NULL != b);
 	assert (NULL != c);
 
-	for (i=0;i<a_arr;++i)
+	for (i = 0; i < a_arr; ++i)
 	{
 		ascii[(int)a[i]]= 1;
 	}
-	for (i=0;i<b_arr;++i)
+	for (i = 0; i < b_arr; ++i)
 	{
 		ascii[(int)b[i]] += 5;
 	}
-	for(i=0; i<c_arr; i++)
+	for(i = 0; i < c_arr; ++i)
 	{
-		ascii[(int)c[i]] =0; 
+		ascii[(int)c[i]] = 0; 
 	}
-	for (i=0;i<ASCII;++i)
+	for (i = 0; i < ASCII; ++i)
 	{
 		if(ascii[i] > 5)
 			printf(" %c ",(char)i);
 	}
+}
+
+
+
+
+static void Reverse(char *str, size_t length)
+{
+	char *start = str;
+	char *end = str + length - 1;
+		
+	while (start < end)
+	{
+		char t = *start;
+		*start = *end; 
+		*end = t;
+		++start;
+		--end;
+	}
+}
+
+
+char *ItoaIntToStrBase(int value, char *str, unsigned int base)
+{
+	char *ptr = str;
+	int i = 0;
+	size_t len = 0;
+	char flag = '\0';
+	
+	assert(NULL != str);
+	
+	if(0 > value)
+	{
+		value= -value;
+		flag = '-';
+	}
+	
+	while(value)
+	{
+		char leftover = value % base;
+
+		if (leftover > 9)
+		{
+			leftover -= DECIM;
+			leftover += CAP_LETTER;
+			*(ptr+i) = leftover;
+		}
+		else
+		{
+			leftover += ZERO;
+			*(ptr + i) = leftover;
+		}
+
+	value /= base;
+	++i;
+	
+	}
+	*(ptr + i) = flag;
+	len = strlen(str);
+	Reverse(str,len);
+	
+	return str;
+}
+
+int AtoiStrToIntBase(const char *nptr, unsigned int base)
+{
+	int num = 0;
+	int i = 0;
+	int sign = 1;
+
+	assert(NULL != nptr);
+	
+	while (isspace(nptr[i]))
+	{
+		++i;
+	}
+	if ( nptr[i] == '-')
+	{
+		sign = -1;
+		++i;
+	}
+
+	while (isalnum(nptr[i]))
+	{
+		
+		if (isdigit(nptr[i]))
+		{
+			num *=  base;
+			num += (nptr[i] - ZERO);
+		}
+		else
+		{
+			num = (num * base) + toupper(nptr[i]) - CAP_LETTER + DECIM;
+		}
+	++i;
+
+	} 
+	
+	return sign * num;		
+}
+	
+
+
+int AtoiStrToInt(const char *nptr)
+{
+	assert(NULL != nptr);
+	return (AtoiStrToIntBase(nptr,10));  
+
+}
+
+
+
+char *ItoaIntToStr(int value, char *str)
+{
+	assert(NULL != str);
+	return ItoaIntToStrBase(value, str, 10);
+	
+}
+
+int IsLittleEndian(void)
+{
+	int num = 1;
+	char *indian = (char *)&num;
+        
+    return (indian[0] == 1);
 }
 
 /* previous version, works but not as efficient
@@ -103,132 +230,3 @@ void PrintFromArrays(char *a, char *b, char *c, size_t a_arr, size_t b_arr, size
 	temp = NULL;
 	
 }*/
-
-
-static void Reverse(char *str, size_t length)
-{
-	char *start = str;
-	char *end = str + length - 1;
-		
-	while (start < end)
-	{
-		char t = *start;
-		*start = *end; 
-		*end = t;
-		++start;
-		--end;
-	}
-}
-
-
-char *ItoaIntToStrBase(int value, char *str, unsigned int base)
-{
-	char *ptr = str;
-	int i = 0;
-	size_t len = 0;
-	char flag = '\0';
-	
-	assert(NULL != str);
-	
-	if(0 > value)
-	{
-		value= -value;
-		flag = '-';
-	}
-	
-	while(value)
-	{
-		char leftover = value % base;
-
-		if (leftover > 9)
-		{
-			leftover -= DECIM;
-			leftover += 'A';
-			*(ptr+i) = leftover;
-		}
-		else
-		{
-			leftover += '0';
-			*(ptr+i) = leftover;
-		}
-
-	value /= base;
-	++i;
-	
-	}
-	*(ptr + i) = flag;
-	len = strlen(str);
-	Reverse(str,len);
-	
-	return str;
-}
-
-int AtoiStrToIntBase(const char *nptr, unsigned int base)
-{
-	int num = 0;
-	int i = 0;
-	int sign = 1;
-
-	assert(NULL != nptr);
-	
-	while (nptr[i] == ' ' || nptr[i] == '\n' || nptr[i] == '\t')
-	{
-		++i;
-	}
-	if ( nptr[i] == '-')
-	{
-		sign = -1;
-		++i;
-	}
-
-	while (isalnum(nptr[i]))
-	{
-		
-		if (isdigit(nptr[i]))
-		{
-			num *=  base;
-			num += (nptr[i] - '0');
-		}
-		if ((nptr[i] >= 'A') && (nptr[i] <= 'Z'))
-		{
-			num *=  base;
-			num += (nptr[i] - 'A' + DECIM);
-		}
-		else if( (nptr[i] >= 'a') && (nptr[i] <= 'z'))
-		{
-			num *=  base;
-			num += (nptr[i] - 'a' + DECIM);
-		}
-
-	++i;
-	} 
-	
-	return sign * num;		
-}
-	
-
-
-int AtoiStrToInt(const char *nptr)
-{
-	assert(NULL != nptr);
-	return (AtoiStrToIntBase(nptr,10));  
-
-}
-
-
-
-char *ItoaIntToStr(int value, char *str)
-{
-	assert(NULL != str);
-	return ItoaIntToStrBase(value, str, 10);
-	
-}
-
-int IsLittleEndian(void)
-{
-	int num = 1;
-	char *indian = (char *)&num;
-        
-    return (indian[0] == 1);
-}
-
