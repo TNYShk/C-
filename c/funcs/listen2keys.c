e92aeb0e7a54c58429db1eb42460c57f13f47b4b
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-01-19 18:25:57 +0200

    small updates

--- c/funcs/listen2keys.c
+++ c/funcs/listen2keys.c
@@ -1,56 +1,65 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h> /* print*/
+#include <stdlib.h> /* system() hide func*/
 
 
 #include "listen2keys.h"
 
-#define UNSC 256
-#define ESC 27
-typedef void (*foo)(char);
+#define ASCIT (256)
+#define ESC (27)
+#define HIDE (system("stty -icanon -echo"))
+#define UNHIDE (system("stty icanon echo");)
 
-void iAT()
+typedef void (*foo)(char);
+typedef void (*ptr_to_func)(void);
+
+static void DoNothing(void);
+
+
+
+
+void iAT(void)
 {
 	char cc=' ';
 	system("stty -icanon -echo");
 	while (cc != ESC)
 	{
-		while((cc !='T') && (cc !='A'))
+		while((cc != 'T') && (cc != 'A'))
 		{
-			cc= getc(stdin);
+			cc = getc(stdin);
 		}
 		printf("%c-pressed! Well hello there\n",cc);
-		cc= getc(stdin);
+		cc = getc(stdin);
 	}
 
 	system("stty icanon echo");
 }
 
-void ifAT()
+void ifAT(void)
 {
-	char cc=' ';
+	char cc =' ';
 	system("stty -icanon -echo");
 	
-	while (cc !=ESC)
+	while (cc != ESC)
 	{
-		cc= getc(stdin);
+		cc = getc(stdin);
 
-		if((cc == 'A') || (cc =='T'))
+		if((cc == 'A') || (cc == 'T'))
 		{
-			printf("%c-pressed! Yey Timo\n",cc);
+			printf("%c-pressed!\n",cc);
 		}
 	}
 	system("stty icanon echo");
 
 }
 
-void PrintAT()
+void PrintAT(void)
 {
 	char cc = ' ';
 	system("stty -icanon -echo");
 	
 	while(1)
 	{
-		cc=getc(stdin);
+		cc = getc(stdin);
 		
 		switch(cc){
 			case (ESC):
@@ -66,31 +75,47 @@ void PrintAT()
 
 void PrintChar(char c)
 {
-	if( c== 'A' || c== 'T')
+	if( c == 'A' || c == 'T')
 	{
 		printf("%c is Bananna also %c pressed\n",c,c);
 	}
 }
 
+static void DoNothing(void)
+{
+	printf("\n\t");
+}
+
 
 void ActionFunc()
 {
-	char cc;
-	int i;
-	foo lut_of[UNSC];
+	char cc = ' ';
+	int i = 0;
+
+	ptr_to_func lut_of[ASCIT];
+	foo lut[ASCIT] = {0};
+
+	ptr_to_func nada = DoNothing;
+	ptr_to_func a_t_pressed = ifAT;
 	
-	for(i=0;i<UNSC;i++)
+
+	for(i = 0;i < ASCIT; ++i)
 	{
-		lut_of[i]=PrintChar;
+		lut_of[i] = nada;
+		lut[i] = PrintChar;
 	}
-	
+	lut_of['A'] = a_t_pressed;
+	lut_of['T'] = a_t_pressed;
+
 	system("stty -icanon -echo");
 	
-	while((cc=getc(stdin)) != ESC)
+	while((cc = getc(stdin)) != ESC)
 	{
-		lut_of[(unsigned char)cc](cc);
+		lut[(unsigned char)cc](cc);
 	}
-
+	
 	system("stty icanon echo");
+	
 }
+
 
--- c/funcs/listen2keys_test.c
+++ c/funcs/listen2keys_test.c
@@ -1,16 +1,13 @@
 #include <stdio.h>  /*     standard library printf and what not  */
-#include <stdlib.h> /*    for stty icanon?                       */
+#include <stdlib.h> 
 
 #include "listen2keys.h"
-
-#define UNSC 256
-typedef void (*foo)(char);
 
 
 int main()  
 {
 
-
+	
 	iAT();
 	
 	ifAT();
