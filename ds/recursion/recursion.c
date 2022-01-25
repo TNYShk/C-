f6b3a56714d8d11ce1f1dbf24925ed547fe6fc7a
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-01-25 15:03:10 +0200

    rec flip done

--- ds/recursion/recursion.c
+++ ds/recursion/recursion.c
@@ -1,24 +1,45 @@
 #include <stdio.h>    /* printf */
 #include <assert.h> /* assert */
 #include <math.h> /* power fabs */
+#include <stdlib.h>
 
+
+typedef struct node
+{
+	void *data;
+	struct node *next;
+}node_t;
+
+
+static void TestFlip();
+static void TestFibo();
 static void PrintRec(int);
 int FibonacciIterate(int element_index);
 int FibonacciRec(int element_index);
-unsigned long GetFibonacci(unsigned int n);
+node_t *FlipRec(node_t *head);
+static char ReverseString(char *input);
+
+
+
+
 
 int main(void)
 {
+	TestFibo();
+	TestFlip();
+	
+	/*ReverseString(" hello my name");
+	printf("\n");
+	
 	PrintRec(5);
+	printf("\n");
 	
-	printf("\nRecursive: %d ",FibonacciRec(5));
-	printf("\nIterative: %d ",FibonacciIterate(5));
-	printf("\nIterative: %d ",FibonacciIterate(40));
-	printf("\nRecursive: %d ",FibonacciRec(40));
-	
+	*/
 	
 	return 0;
 }
+
+
 
 
 static void PrintRec(int val)
@@ -48,28 +69,6 @@ int FibonacciIterate(int element_index)
 	return ans;
 }
 
-unsigned long GetFibonacci(unsigned int n)
-{
-	int index = 2;
-	int ans = 1;
-	int *fibo = (int *)calloc(1+n,sizeof(size_t));
-	assert (fibo != NULL);
-	fibo[0] = 1;
-	fibo[1] = 1;
-
-	while(index <= n)
-	{
-		fibo[index] = fibo[index-1] + fibo[index-2];
-		++index;
-	
-	}
-	
-	ans = fibo[n-1];
-
-	free(fibo);
-	fibo = NULL;
-	return ans;
-}
 
 int FibonacciRec(int element_index)
 {
@@ -78,4 +77,72 @@ int FibonacciRec(int element_index)
 		return element_index;
 	}
 	return (FibonacciRec(element_index - 1) + FibonacciRec(element_index - 2) );
+}
+
+static char ReverseString(char *input)
+{
+	if( '\0' == *input)
+		return *input;
+	
+	++input;
+	return (ReverseString(input)+ printf("%c", *input));
+}
+
+
+node_t *FlipRec(node_t *head)
+{
+	node_t *new;
+	if((NULL == head) || (NULL == head->next))
+	{
+		return head;
+	}
+	new = FlipRec(head->next);
+	head->next->next = head;
+	head->next = NULL;
+	return new;
+}
+
+static void TestFlip()
+{
+	int x = 1;
+	int y = 2;
+	int z = 3;
+	int w = 4;
+	
+
+	node_t *head = NULL;
+
+	node_t one;
+	node_t two; 
+	node_t three; 
+	node_t four; 
+
+	one.data = &x;
+	one.next = &two;
+
+	two.data = &y;
+	two.next = &three;
+	three.data = &z;
+	three.next = &four;
+	four.data = &w;
+	four.next = NULL;
+	
+	head = &one;
+	printf("\n\t----------------------------Recursive Flip-------------------------------------------\n");
+	printf("before: data in two.next->data is %d\n", *(int *)two.next->data);
+	printf("before: data in three.next->data is %d\n", *(int *)three.next->data);
+	FlipRec(head);
+	printf("post flip data in two.next->data is %d\n", *(int *)two.next->data);
+	printf("post flip data in three.next->data is %d\n", *(int *)three.next->data);
+}
+
+static void TestFibo()
+{
+	printf("\n\t-----------------------Fibonacci, get element----------------------------------------\n");
+	printf("\nRecursive(5): %d ",FibonacciRec(5));
+	printf("\nIterative(5): %d ",FibonacciIterate(5));
+	printf("\n");
+	printf("\nIterative (40): %d ",FibonacciIterate(40));
+	printf("\nRecursive(40): %d ",FibonacciRec(40));
+	printf("\n");
 }
