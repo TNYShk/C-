5ddc2be028bba396efbfe3e4c1bed8290057332b
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-01-27 21:00:28 +0200

    adding tests

--- ds/avl/avl_test.c
+++ ds/avl/avl_test.c
@@ -18,6 +18,10 @@ int CompareData(const void *left, const void *right);
 int PrintNodes(void *data, void *param);
 int DontPrintNodes(void *data, void *param);
 
+void CreateDestroy();
+void SizeHeight();
+
+
 int main(void)
 {
 	avl_t *avl = NULL;
@@ -26,6 +30,9 @@ int main(void)
 	int num3 = 5;
 	int num4 = 10;
 	int num5 = 11;
+
+	CreateDestroy();
+	SizeHeight();
 	avl = AVLCreate(CompareData);
 	
 	(1 == AVLIsEmpty(avl)? printf("empty tree!\n"): printf("NOT empty tree!\n") ); 
@@ -61,6 +68,80 @@ int main(void)
 
 	return 0;
 }
+
+void CreateDestroy()
+{
+
+	avl_t *avl = NULL;
+	avl_t *avl1 = NULL;
+	int num = 6;
+	int num2 = 18;
+	int num3 = 5;
+	int num4 = 10;
+	int num5 = 11;
+	int num6 = 110;
+
+	printf("\n\tCreate & Destroy Tests\n");	
+	avl1 = AVLCreate(CompareData);
+	assert(1 == AVLIsEmpty(avl1));
+	AVLDestroy(avl1);
+
+	avl = AVLCreate(CompareData);
+	assert(1 == AVLIsEmpty(avl));
+	(0 == AVLInsert(avl, &num)? printf("inserted data tree!\n"): printf("Failed!\n") );
+	assert(0 == AVLInsert(avl, &num2));
+	/*assert(0 != AVLInsert(avl, &num2)); WORKS, assert same number */
+	assert(0 == AVLInsert(avl, &num3));
+	assert(0 == AVLInsert(avl, &num4));
+	assert(0 == AVLInsert(avl, &num5));
+	assert(0 == AVLInsert(avl, &num6));
+	assert(6 == AVLSize(avl));
+	assert(0 == AVLForEach(avl, PrintNodes, &num, IN_ORDER));
+	AVLDestroy(avl);
+
+	printf("\n\tCreate & Destroy & IsEmpty PASSED\n");
+
+}
+
+void SizeHeight()
+{
+	avl_t *avl = NULL;
+	int num = 16;
+	int num2 = 18;
+	int num3 = 5;
+	int num4 = 10;
+	int num5 = 11;
+	int num6 = 12;
+
+	printf("\n\tBST- rec size & Height Tests\n");
+
+	avl = AVLCreate(CompareData);
+	assert(1 == AVLIsEmpty(avl));
+	(0 == AVLInsert(avl, &num)? printf("inserting data int tree!\n"): printf("Failed!\n") );
+	assert(1 == AVLHeight(avl));
+	assert(1 == AVLSize(avl));
+	assert(0 == AVLInsert(avl, &num2));
+	assert(2 == AVLHeight(avl));
+	assert(0 == AVLInsert(avl, &num3));
+	assert(0 == AVLInsert(avl, &num4));
+	assert(3 == AVLHeight(avl));
+	
+	assert(0 == AVLInsert(avl, &num5));
+	assert(0 == AVLInsert(avl, &num6));
+	assert(5 == AVLHeight(avl));
+	assert(6 == AVLSize(avl));
+	printf("\n");
+	assert(0 == AVLForEach(avl, PrintNodes, &num, PRE_ORDER));
+	printf("\n");
+	assert(1 == AVLForEach(avl, DontPrintNodes, &num, IN_ORDER));
+	printf("\n");
+	assert(0 == AVLForEach(avl, PrintNodes, &num, POST_ORDER));
+	printf("\n");
+	AVLDestroy(avl);
+	printf("\n\tTree Size & Height PASSED\n");
+}
+
+
 
 
 int CompareData(const void *left, const void *right)
