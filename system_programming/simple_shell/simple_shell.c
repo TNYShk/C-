33912a7c30d7bbf070f985ddb6a3a635b3be4a89
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-02-17 16:41:12 +0200

    fork?

--- system_programming/simple_shell/simple_shell.c
+++ system_programming/simple_shell/simple_shell.c
@@ -9,6 +9,10 @@
 #include <stdlib.h> /*size_t */
 #include <string.h> /* strcmp */
 #include <assert.h> /*assert */
+#include <unistd.h>
+#include <sys/types.h>  /* pid_t */
+#include <sys/wait.h>
+#include <errno.h> /* errno */
 
 typedef int (*compare_func)(const char *,const char *s);
 typedef int (*action_func)(char *dowhat, char *name);
@@ -40,7 +44,7 @@ typedef enum status
 static void InitStruct(shell_t *array);
 int DoExit(char *dowhat, char *name);
 int DoSystem(char *dowhat, char *name);
-
+int DoFork(char *dowhat, char *name);
 
 
 
@@ -95,7 +99,7 @@ static void InitStruct(shell_t *array)
 
 	array[FORK].name = "fork\n";
 	array[FORK].cmp = strcmp;
-	array[FORK].action = DoSystem;
+	array[FORK].action = DoFork;
 
 
 }
@@ -117,18 +121,68 @@ int DoSystem(char *dowhat, char *name)
 	int return_value = 1;
 	(void)name;
 	assert(NULL != dowhat);
-	printf("enter a command:\n");
-	
+	printf("In System, enter commands:\n");
+
 	dowhat = fgets(dowhat, MAXLENG, stdin);
 
 	while (0 != strcmp("exit\n", dowhat))
 	{
 		return_value = system(dowhat);
-		printf("returned value : %d\n", return_value);
-
 		dowhat = fgets(dowhat, MAXLENG, stdin);
 	}
 	
 	return return_value;
 	
+}
+
+
+int DoFork(char *dowhat, char *name)
+{	
+	int status = 0;
+	char **str_arr = NULL;
+	char **head = NULL;
+	(void)name;
+
+	assert(NULL != dowhat);
+
+    while (1)
+    {
+        printf("enter an input\n");
+
+        dowhat = fgets(dowhat, MAXLENG, stdin);
+        if(0 == strcmp("exit\n", dowhat))
+        {
+            break;
+        }
+        
+        str_arr = (char **)calloc(strlen(dowhat), sizeof(char *));
+        if (NULL == str_arr)
+        	return -1;
+
+        head = str_arr;
+        
+        *str_arr++ = strtok(dowhat, " \n");
+        
+        while (NULL != (*str_arr++ = strtok(NULL, " \n")));
+       
+
+        if (0 == fork())
+        {
+            errno = 0;
+            if (-1 == execvp(*head, head) && errno != 0)
+            {
+                printf("failed, errno = %d \n", errno);
+            }
+        }
+        else
+        {
+            wait(&status);
+        }
+        free(head);
+        head = NULL;
+
+    }
+
+
+    return 0;
 }
