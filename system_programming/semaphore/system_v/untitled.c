0ad1b5a4eaa00f23ecd1710792d7d50ae5c5e6e7
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-02-27 14:02:41 +0200

    sys v die

--- system_programming/semaphore/system_v/untitled.c
+++ system_programming/semaphore/system_v/untitled.c
@@ -20,12 +20,18 @@
 #define errExit(msg) do { perror(msg); exit(EXIT_FAILURE); } while (0)
 
 #include "semaphore_sys_v.h"
-#define FAIL (-1)
+#define FILE_NAME ("/semaphore_sys_v.h")
+#define FAIL_SEMA (-1)
+
 #define LAZY_ASCII (128)
-#define EXIT (5)
+#define FAIL (-1)
+#define INVALID_INPUT (1)
+#define OK_EXIT (2)
 #define SUCCESS (0)
 typedef int (*sem_act_func)(int semid, char command);
 static sem_act_func sem_actions[LAZY_ASCII] = {NULL};
+
+
 
 union semun 
 {
@@ -36,10 +42,11 @@ union semun
     struct seminfo *__buf;
 #endif
 };
-
-
+static int IncrementSema(int sema_id , int incrent_by , int undo_flag);
+static int GetSemaid(const unsigned char id, int value);
 static void InitSemAct(void);
 static int DoExit(int semid, char command);
+static int DoNothing(int semid, char command);
 static int DoView(int semid, char command);
 static int DoUnlink(int semid, char command);
 static int DoDecrement(int semid, char command);
@@ -47,39 +54,34 @@ static int DoIncrement(int semid, char command);
 
 int main(int argc, const char *argv[])
 {
-    
     SysVSemManipulation(argv);
     return 0;
 }
 
 
+
+
+
+
 int SysVSemManipulation(const char **cmd)
 {
-    struct semid_ds ds ;
-    union semun arg;
-    int semid = 0, j = 0;
+    int semid = 0;
+    int return_val = 0;
     int charly = ' ';
-    int status = 0;
-    key_t key = ftok(cmd[1], j);
-    if(FAIL == key)
-    {
-        errExit("key_failed");
-    }
+    int status = SUCCESS;
+    assert(NULL != cmd[1]);
+    assert(NULL != cmd[2]);
 
-    semid = semget(key, 1, IPC_CREAT);
-    if(FAIL == semid)
+    semid = GetSemaid(atoi(cmd[1]),atoi(cmd[2]));
+    if (FAIL_SEMA == semid)
     {
         errExit("semget");
     }
 
-    arg.val = atoi(cmd[2]);
-    if (FAIL == semctl(semid, 0, SETVAL, arg))
-    {
-        errExit("semctl");
-    }
     InitSemAct();
+
     printf("please enter letter: I,D,V,R or X\n");
-    while((charly != 'X') && (status != EXIT))
+    while((charly != 'X') && (status != OK_EXIT))
     {
         charly = getc(stdin);
         status = sem_actions[charly](semid, charly);
@@ -87,12 +89,45 @@ int SysVSemManipulation(const char **cmd)
     return status;
 }
 
+
+static int GetSemaid(const unsigned char id, int value)
+{
+    key_t sema_key = 0;
+    int sema_id = 0;
+
+    char cwd[LAZY_ASCII] = {0};
+
+    if (getcwd(cwd, sizeof(cwd)) == NULL) 
+    {
+        return EXIT_FAILURE; 
+    }
+
+    strcat(cwd,FILE_NAME);
+    
+    sema_key = ftok(cwd , id);
+
+    sema_id = semget(sema_key, value , IPC_CREAT | IPC_EXCL | 0666); 
+    if (errno == EEXIST)
+    {
+        sema_id = semget(sema_key, value , 0);
+    }
+    else
+    {
+        IncrementSema(sema_id , value ,0);
+    }
+
+    return sema_id;
+}
+
+
+
+
 static void InitSemAct(void)
 {
     size_t counter = 0;
     for(;counter < LAZY_ASCII; ++counter)
     {
-        sem_actions[counter] = NULL;
+        sem_actions[counter] = &DoNothing;
     }
     sem_actions['X'] = &DoExit;
     sem_actions['V'] = &DoView;
@@ -106,22 +141,51 @@ static int DoExit(int semid, char command)
 {
     (void)semid;
     (void)command;
-    return EXIT;
+    return OK_EXIT;
 }
 
 
+static int DoNothing(int semid, char command)
+{
+    (void)semid;
+    (void)command;
+    return INVALID_INPUT;
+}
 
 
 static int DoView(int semid, char command)
 {
-    union semun arg = {0};
-    printf("Semaphore value is: %d\n", semctl(semid, 0, GETVAL, arg));
+    int value = 0;
+    value = semctl(semid, 0, GETVAL);
+    if(FAIL_SEMA == value)
+        errExit("sem value");
+    printf("Semaphore value is: %d\n", value);
     
     (void)command;
 
     return SUCCESS;
 
 }
+
+static int IncrementSema(int sema_id , int incrent_by , int undo_flag)
+{
+    struct sembuf op_settings = {0};
+
+    assert(incrent_by >= 0);
+
+    op_settings.sem_op = incrent_by;
+    op_settings.sem_flg = 0;
+    op_settings.sem_num = 0;
+
+    if (undo_flag)
+    {
+        op_settings.sem_flg = SEM_UNDO;
+    }
+    
+    return (FAIL_SEMA == semop(sema_id , &op_settings ,1)) ? EXIT_FAILURE : EXIT_SUCCESS ;
+}
+
+
 
 static int DoIncrement(int semid, char command)
 {
@@ -166,7 +230,7 @@ static int DoUnlink(int semid, char command)
         errExit("semctl_remove");
     }
     printf("removed?\n");
-    return EXIT;
+    return OK_EXIT;
 }
 
 
