a50d5bb3154b4131b9a75ad2b63bb1c99a244cf4
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-03-14 19:38:41 +0200

    no class here

--- java/HelloWorldApp.java
+++ java/HelloWorldApp.java
@@ -5,56 +5,46 @@ import HelloWorldApp.MySecondClass;
 
 public class HelloWorldApp {
     
-    public static void main(String[] args) 
-    { 
-       Runtime something = Runtime.getRuntime();
-       long memort1;
+   public static void main(String[] args) { 
+      Runtime something = Runtime.getRuntime(); 
+      long memort1 = 0;
       System.out.println("Hello, World");
         
-       Date today = new Date();
-        System.out.println(today); 
+      Date today = new Date();
+      System.out.println(today); 
 
-       
-       MySecondClass.foo1(); 
+      MySecondClass.foo1(); 
       /*  MySecondClass.foo2();   */
-       int j = 0;
-    
-     
-     List<int[]> arrayList = new ArrayList<int[]>();
-    
-     for(int i = 0; i <1000; i++)
-     {
-        int[] hundK = new int[100000];
-        for(; j< 100000; j++)
-        {
-           hundK[j] = j*10;
-        }
-        
-        arrayList.add(i, hundK);
-       
-     }
-    /*  System.out.println(arrayList); */
-     System.out.println("total memory is: "+ something.totalMemory());
-     memort1 = something.freeMemory();
-     System.out.println("init free memory is: "+ memort1);
-     something.gc();
-     memort1 = something.freeMemory();
-     System.out.println("free memory after gc is: "+ memort1);
-    
       
-     Recursive(0);
+       QFiveTeen();
+     
+    /*  System.out.println(arrayList); */
+      System.out.println("total memory is: "+ something.totalMemory());
+      memort1 = something.freeMemory();
+      System.out.println("init free memory is: "+ memort1);
+      something.gc();
+      memort1 = something.freeMemory();
+      System.out.println("free memory after gc is: "+ memort1);
+      System.out.println(" ");
+      
+     MySecondClass.Recursive(0);
     }
    
-   static void Recursive(int something)
-   {
-      if(100 == something)
+   public static void QFiveTeen(){
+
+      List<int[]> arrayList = new ArrayList<int[]>();
+      int j =0;
+      for(int i = 0; i <1000; i++)
       {
-         System.out.print(something + " " );
-         return;
-      }
+         int[] hundK = new int[100000];
+         for(; j< hundK.length; j++)
+         {
+            hundK[j] = j*10;
+         }
          
-    System.out.print(something + " " );
-      Recursive(something +1 );
+         arrayList.add(i, hundK);
+      }
+     
    }
 }
 
--- java/HelloWorldApp/MySecondClass.java
+++ java/HelloWorldApp/MySecondClass.java
@@ -7,7 +7,17 @@ public class MySecondClass {
         System.out.println("Something Dark");
     }  
     
-    void foo2(){
-        System.out.println("Something Light");
+   /*  void foo2(){
+        System.out.println("Something Light"); */
+    
+    public static void Recursive(int something){
+      
+        if(100 == something){
+        System.out.print(something + " " );
+        return;
     }
+         
+    System.out.print(something + " " );
+    Recursive(something +1 );
+   }
 }
