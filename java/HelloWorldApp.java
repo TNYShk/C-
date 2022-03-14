05fce4599cddf94f965472b6fc8c7f5835b7a6bb
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-03-14 07:40:20 +0200

    java

---
+++ java/HelloWorldApp.java
@@ -0,0 +1,9 @@
+
+import java.util.Date;
+public class HelloWorldApp {
+    public static void main(String[] args) { 
+        System.out.println("Hello, World");
+        Date today = new Date();
+        System.out.println(today);
+    }
+}
