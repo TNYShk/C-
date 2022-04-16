03adc1a0c9cd4d4e7f10a7f6e8f5112e47e62141
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-04-16 11:46:54 +0300

    "generics practice"

--- fs/Excercies/src/il/co/ilrd/generics/ItemTwentySeven.java
+++ fs/Excercies/src/il/co/ilrd/generics/ItemTwentySeven.java
@@ -1,31 +1,32 @@
 package il.co.ilrd.generics;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
+import java.util.*;
+
+//No!
+@SuppressWarnings("unchecked")
 public class ItemTwentySeven<T> {
     T elements;
     int size;
     private final T[] choiceArray;
-    private List<T> myArr;
+    private final List<T> myArr;
 
-
+    //this is not ok, don't do it on a constructor!
+    @SuppressWarnings("unchecked")
     public ItemTwentySeven(Collection<T> elements) {
 
         this.elements = (T) elements;
-        this.size = 15;
+        this.size = 20;
         myArr = new ArrayList<>(elements);
 
         choiceArray = (T[]) elements.toArray();
     }
 
-    public <T> T[] toArray(T[] t) {
+   public <T> T[] toArray(T[] t) {
         if (t.length < size) {
             //this cast is ok since the array created is of the same type as the one passed (T[]
             @SuppressWarnings("unchecked") T[] result =
-                    (T[]) Arrays.copyOf(choiceArray, size, t.getClass());
+             (T[]) Arrays.copyOf(choiceArray, size, t.getClass());
             return result;
         }
         System.arraycopy(choiceArray, 0, t, 0, size);
@@ -36,12 +37,12 @@ public class ItemTwentySeven<T> {
         return t;
     }
 
+
+
     @Override
     public boolean equals(Object other) {
-        if ((ItemTwentySeven.class.isInstance(other))) {
-
-              @SuppressWarnings("unchecked")
-              ItemTwentySeven<T> otherPair = (ItemTwentySeven<T>) other;
+        if ((other instanceof ItemTwentySeven)) {
+            ItemTwentySeven<T> otherPair = (ItemTwentySeven<T>) other;
 
             return ((this.elements.equals(otherPair.elements)) && (this.myArr.equals(otherPair.myArr)));
         }
@@ -50,15 +51,14 @@ public class ItemTwentySeven<T> {
 
     @Override
     public String toString(){
-        return (this.elements.toString() + " \n size: " + this.size + " \n" + this.myArr.toString());
+        return (this.elements.toString() );
+    }
+    @Override
+    public int hashCode(){
+        return this.elements.hashCode();
     }
 
-    @Override
-    public boolean equalsss(Object other) {
-        if (!(other instanceof Pair)) {
-            return false;
-        }
-        return this.key.equals(((Pair<K, V>)other).key) &&
-                this.value.equals(((Pair<K, V>)other).value);
-    }
+
+
+
 }
