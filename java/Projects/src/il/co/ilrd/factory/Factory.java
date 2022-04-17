04585ab11b4ab64632158298559c4b304cd954db
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-04-17 19:50:41 +0300

    "factory updated tests"

---
+++ java/Projects/src/il/co/ilrd/factory/Factory.java
@@ -0,0 +1,24 @@
+package il.co.ilrd.factory;
+
+import java.util.HashMap;
+
+import java.util.function.Function;
+public class Factory<K, T, D> {
+    private HashMap<K, Function<D,? extends T>> factoryFun = new HashMap<>();
+
+    public void add(Function<D,? extends T> funObject, K key){
+        factoryFun.put(key,funObject);
+    }
+
+    public T create(K key){
+        return factoryFun.get(key).apply(null);
+    }
+
+    public T create (K key, D data){
+        return factoryFun.get(key).apply(data);
+    }
+
+    public int mapSize(){
+        return this.factoryFun.size();
+    }
+}
