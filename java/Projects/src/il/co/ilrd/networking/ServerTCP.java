852fa8b6254f8beddb375d523741fd055c269f9e
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-05-10 20:25:10 +0300

    "tcp ping pong headache"

--- java/Projects/src/il/co/ilrd/networking/ServerTCP.java
+++ java/Projects/src/il/co/ilrd/networking/ServerTCP.java
@@ -1,63 +1,79 @@
 package il.co.ilrd.networking;
 
-import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.SocketException;
-import java.util.concurrent.atomic.AtomicBoolean;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
 
-public class ServerTCP implements Runnable{
-    private ClientTCP client;
-    private DatagramSocket serverSocket;
-    private Thread run, tick,send, receive;
+public class ServerTCP implements Runnable {
+
+
+    private InputStream input;
     private int port;
-    private AtomicBoolean isRun = new AtomicBoolean();
+    private volatile boolean isRun;
+    private Socket socket;
 
-    public ServerTCP(int port){
+    public ServerTCP(int port) throws IOException {
         this.port = port;
-        try{
-            serverSocket = new DatagramSocket(port);
-        }catch(SocketException e){
+        try (ServerSocket serverSocket = new ServerSocket(port, 1)) {
+            socket = serverSocket.accept();
+            System.out.println("Server is listening on port " + port);
+
+        }
+
+    }
+
+    public void listenToClient() throws IOException {
+        try {
+            while (isRun) {
+                input = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                String line = reader.readLine();
+                System.out.println(line);
+                if (line.equals("ping"))
+                    sendDataToClient();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+
+    }
+
+    public void sendDataToClient() throws IOException {
+        OutputStream output = socket.getOutputStream();
+        PrintWriter writer = new PrintWriter(output, true);
+        writer.println("pong");
+    }
+
+    public void closing() {
+        isRun = false;
+        //socket.close();
+    }
+
+    public static void main(String[] args) {
+        ServerTCP tennisServer = null;
+        try {
+            tennisServer = new ServerTCP(8080);
+        } catch (IOException e) {
             e.printStackTrace();
         }
-        run = new Thread(this, "server");
-        run.start();
+        tennisServer.run();
+
     }
 
     @Override
     public void run() {
-        isRun.set(true);
-        System.out.println("server started! " + port);
-        tick();
-        receive();
+        isRun = true;
+        try {
+            listenToClient();
+            Thread.currentThread().sleep(5000);
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        } finally {
+            closing();
+        }
+
     }
 
-    private void tick(){
-        tick = new Thread(() -> {
-            while(isRun.get()){}
-        });
-        tick.start();
-    }
-
-    private void receive(){
-        receive = new Thread(){
-            public void run(){
-                while(isRun.get()){
-                    byte[] data = {'p','o','n','g'};
-                    DatagramPacket packet = new DatagramPacket(data, data.length);
-                    try {
-                        serverSocket.receive(packet);
-                    }catch(IOException e) {
-                        e.printStackTrace();
-                    }
-                    read(packet);
-                }
-            }
-        };
-        receive.start();
-    }
-    private void read(DatagramPacket packet) {
-        String message = new String(packet.getData());
-        System.out.println(message);
-    }
 }
