2dd1b604f7680e72f11528f1facdb4b21fe63d1a
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-05-14 20:04:07 +0300

    "added Miriam Webster thesaurus API"

--- java/Projects/src/il/co/ilrd/networking/ClientTCP.java
+++ java/Projects/src/il/co/ilrd/networking/ClientTCP.java
@@ -33,7 +33,7 @@ public class ClientTCP {
       // connectToServer("10.111.0.10",26666);
         //connectToServer("10.1.0.116",12212);
        // connectToServer("10.10.1.32",50001);
-        connectToServer("192.168.68.101",26666);
+        connectToServer("192.168.68.101",10523);
     }
 
 }
