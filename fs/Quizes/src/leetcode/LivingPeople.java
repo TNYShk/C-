ee50cd28f976b3c45b4f07c219c105c6c3069e7e
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-06-13 06:58:50 +0300

    "stuff"

--- fs/Quizes/src/leetcode/LivingPeople.java
+++ fs/Quizes/src/leetcode/LivingPeople.java
@@ -2,7 +2,7 @@ package leetcode;
 
 import java.util.*;
 
-//given a list of people with the DoB and DoD, compute the year with the most number of people alive, assuming all born between 1900-2000 included
+//given a list of people with the DoB and DoD, compute the year with the most number of people alive, assuming all born between 1900-1999 included
 public class LivingPeople {
     public Person[] population;
 
@@ -12,14 +12,9 @@ public class LivingPeople {
     public LivingPeople(int many){
         population = new Person[many];
         for(int i = 0; i< population.length;++i){
-            int birthday = (int) Math.floor(Math.random() *(2000-1900)+ 1900);
-            int deathhday = (int) Math.floor(Math.random() * (2000-birthday+1)+ birthday);
+            int birthday = (int) Math.floor(Math.random() *(1999-1900)+ 1900);
+            int deathhday = (int) Math.floor(Math.random() * (1999-birthday+1)+ birthday);
             population[i] = new Person(birthday,deathhday);
-        }
-    }
-    public void printDeaths(){
-        for(int i= 0;i<population.length;++i){
-            System.out.println("person "+ i+ " died in "+ population[i].getDeathYear());
         }
     }
 
@@ -48,6 +43,56 @@ public class LivingPeople {
         }
 
     }
+    private int[] getLiving(Person[]people){
+        int[]year = new int[people.length];
+        for(int i=0; i< population.length;++i){
+            year[i] = population[i].getBirth();
+        }
+        Arrays.sort(year);
+        return year;
+    }
+    private int[] getLDeath(Person[]people){
+        int[]year = new int[people.length];
+        for(int i=0; i< population.length;++i){
+            year[i] = population[i].getDeathYear();
+        }
+        Arrays.sort(year);
+        return year;
+    }
+
+
+    public int getLivingYear(Person[] people) {
+        int[] live = getLiving(people);
+        int[] death = getLDeath(people);
+
+        LinkedHashMap<Integer, Integer> bestMap = new LinkedHashMap<>();
+        bestMap.put(live[0],1);
+        for (int i = 1; i < people.length; ++i) {
+            Integer current = bestMap.get(live[i-1]);
+            bestMap.merge(live[i],current+1,Integer::sum);
+
+        }
+        for(int i = 1; i<people.length;++i){
+            Integer current = bestMap.get(live[i-1]);
+            //bestMap.put(bestMap.get(i),current-1);
+            bestMap.merge(death[i], current-1, Integer::sum);
+        }
+
+       return (getBestfromHash(bestMap));
+    }
+    private int getBestfromHash(Map<Integer,Integer>map) {
+        int max = Collections.max(map.values());
+
+        int bestYear = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue().equals(max)) {
+                bestYear = entry.getKey();
+                break;
+            }
+        }
+        return bestYear;
+    }
+
 
     public int mostPeopleAlive(){
         HashMap<Integer,Integer> years = new HashMap<>(100);
@@ -57,24 +102,31 @@ public class LivingPeople {
                 years.merge((2000-j),1,Integer::sum);
             }
         }
-        int key = Collections.max(years.values());
-        int maxYear = 0;
-       /* System.out.println(years.keySet());
-        System.out.println(years.values());*/
-        for(Map.Entry<Integer,Integer> entry: years.entrySet()){
-            if(entry.getValue().equals(key))
-                maxYear = entry.getKey();
-        }
-        return 2000- maxYear;
+       return 2000- getBestfromHash(years);
     }
 
     public static void main(String[] args){
-        LivingPeople test = new LivingPeople(3);
+        LivingPeople test = new LivingPeople(5);
 
         test.printBirthsDeaths();
-        System.out.println(test.mostPeopleAlive());
+        System.out.println("intuitive solution: "+test.mostPeopleAlive());
+        System.out.println("improved: "+test.getLivingYear(test.population));
 
+       System.out.println( test.getUniqueChar("boyswillbeboys"));
     }
 
+    public char getUniqueChar(String str){
+        LinkedHashMap<Character,Integer>word = new LinkedHashMap<>(str.length());
+        for(int i=0; i<str.length();++i){
+            word.merge(str.charAt(i),1,Integer::sum);
+        }
+        for(Map.Entry<Character,Integer>entry: word.entrySet()){
+            if(entry.getValue()==1) {
+                Character cc = (entry.getKey());
+                return cc;
+            }
+        }
 
+    return ' ';
+    }
 }
--- fs/Quizes/src/leetcode/MatrixMaze.java
+++ fs/Quizes/src/leetcode/MatrixMaze.java
@@ -14,12 +14,12 @@ public class MatrixMaze {
             {1,0,1,0},
             {0,0,0,0},
     };
-    static boolean[][]beenThere = new boolean[maze[0].length][maze.length];
+    //static boolean[][]beenThere = new boolean[maze[0].length][maze.length];
 
     public boolean exitMaze(Point enter, Point exit){
-        List<Point> finalPath = getPath(enter);
+        List<Point> finalPath = getPath(enter,exit);
 
-        //System.out.println(finalPath);
+        System.out.println(finalPath);
     //return true;
    return finalPath.contains(exit);
     }
@@ -37,40 +37,44 @@ public class MatrixMaze {
         }
     }
 
-    private void markLocation(Point current){
-        beenThere[current.row][current.col] = true;
+    private void markLocation(boolean[][]map,Point current){
+        map[current.row][current.col] = true;
     }
 
-    public List<Point> getPath(Point start){
-       List<Point> path = new LinkedList<>();
-        if(maze == null || maze.length == 0)
-            return null;
+    public List<Point> getPath(Point start, Point exit){
+        boolean[][]beenThere = new boolean[maze[0].length][maze.length];
+        List<Point> paths = new LinkedList<>();
 
 
-        if(getPath(start.row,start.col,path)){
-            return path;
+        if(getPath(start.row,start.col,paths,exit,beenThere)){
+            return paths;
         }
-        for(Point i: path){
+       /* for(Point i: paths){
             i.getPoint();
-        }
+        }*/
        // System.out.println("here");
-        return path;
+        return paths;
     }
 
-    private boolean getPath(int row,int col, List<Point> path){
+    private boolean getPath(int row,int col, List<Point> path, Point exit, boolean[][]beenThere){
+        if((exit.col == col) && (exit.row == row))
+            return true;
+
         if((col < 0) || (row < 0) || maze[row][col] == 1 || (col== maze.length) || (row== maze[0].length-1) || beenThere[row][col]){
             System.out.println("false");
             return false;
         }
         Point p = new Point(row,col);
         //path.add(p);
-        markLocation(p);
+        markLocation(beenThere,p);
 
-        if(getPath(row,col+1,path) || getPath(row+1,col,path)){
+        if(getPath(row,col+1,path,exit,beenThere) || getPath(row+1,col,path,exit,beenThere)){
             path.add(p);
+            System.out.println("mm");
             return true;
         }
-        if(getPath(row,col-1,path) || getPath(row-1,col,path)){
+        if(getPath(row,col-1,path,exit,beenThere) || getPath(row-1,col,path,exit,beenThere)){
+            System.out.println("mmmm");
             path.add(p);
             return true;
         }
