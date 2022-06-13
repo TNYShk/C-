e9fc1632b25f294ae0b80a74ba1f4716db7fc61e
tanya.shkolnik <tatyanna.shk@gmail.com>
2022-06-13 17:48:58 +0300

    "stuff"

--- fs/Quizes/src/leetcode/LRUCache.java
+++ fs/Quizes/src/leetcode/LRUCache.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 public class LRUCache {
     private final int cap;
-   // private Deque<Integer> keyQ;
+
     private LinkedHashMap<Integer,Integer> lru;
 
     public LRUCache(int capacity) {
@@ -18,13 +18,14 @@ public class LRUCache {
             cap = capacity;
         }
         lru = new LinkedHashMap<>(cap);
-
     }
-
+    private LRUCache(){
+            this(16);
+    }
     public int get(int key) {
         if(!lru.containsKey(key))
             return -1;
-        Integer val =  lru.get(key);
+        Integer val = lru.get(key);
 
        lru.remove(key);
        lru.put(key,val);
@@ -34,9 +35,7 @@ public class LRUCache {
     }
 
     public void put(int key, int value) {
-        if(lru.containsKey(key)){
-            lru.remove(key);
-        }
+        lru.remove(key);
         lru.put(key,value);
 
         if(lru.size() > cap) {
@@ -56,7 +55,18 @@ public class LRUCache {
 
     public static void main(String[] args){
         LRUCache test = new LRUCache(2);
+        test.put(1, 1);
+        test.put(2, 2);
+        Assert.assertEquals(1, test.get(1));
+        test.put(3, 3);
         Assert.assertEquals(-1, test.get(2));
+        test.put(4, 4);
+        Assert.assertEquals(-1, test.get(1));
+        Assert.assertEquals(3, test.get(3));
+        Assert.assertEquals(4, test.get(4));
+        test.printMap();
+
+ /*       Assert.assertEquals(-1, test.get(2));
         test.put(2, 6);
         test.printMap();
         Assert.assertEquals(-1, test.get(1));
@@ -66,24 +76,10 @@ public class LRUCache {
         Assert.assertEquals(test.get(1),2);
         test.printMap();
 
-        Assert.assertEquals( test.get(2),6);
+        Assert.assertEquals( test.get(2),6);*/
 
     }
-  /*  @Test
-    public void Uno() {
-    LRUCache test = new LRUCache(2);
-    test.put(1, 1);
-    test.put(2, 2);
-    Assert.assertEquals(1, test.get(1));
-    test.put(3, 3);
-    Assert.assertEquals(-1, test.get(2));
-    test.put(4, 4);
-    Assert.assertEquals(-1, test.get(1));
-    Assert.assertEquals(3, test.get(3));
-    Assert.assertEquals(4, test.get(4));
-    test.printMap();
-    test.printKeyQ();
-    }*/
+
 }
 
 
